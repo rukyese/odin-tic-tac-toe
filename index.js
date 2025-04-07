082688f18b01e7a4a8a7ff733cd3750f59739fdf
Oghenerukevwe Esemitodje <o.esemitodje@gmail.com>
2025-04-07 23:07:27 +0100

    Create UI

--- index.js
+++ index.js
@@ -5,46 +5,81 @@ const Gameboard = (function () {
     [null, null, null],
   ];
 
+  return { board };
+})();
+
+const Game = (function () {
+  const tiles = document.querySelectorAll(".tiles");
+  const resetBtn = document.querySelector(".reset");
+
   const tokens = ["X", "O"];
   let turnCount = 0;
   let currentTurn = tokens[turnCount % 2];
 
+
+  const placeToken = (event) => {
+      const rowIndex = Math.floor(event.target.dataset.target / 3);
+      const colIndex = event.target.dataset.target % 3;
+      play(rowIndex, colIndex);
+  };
+
+  const displayResults = (result) => {
+    const results = document.querySelector(".results");
+    results.style.display = "block";
+    if (result === "win") {
+      results.textContent = `${currentTurn} wins`;
+      return;
+    };
+    if (!result) {
+      results.textContent = "";
+      results.style.display = "none";
+      return;
+    }
+    results.textContent = "Draw";
+    results.classList.add("draw");
+  };
+
   const checkWin = () => {
     const win =
-      (board[0][0] &&
-        board[0][0] === board[0][1] &&
-        board[0][1] === board[0][2]) ||
-      (board[1][0] &&
-        board[1][0] === board[1][1] &&
-        board[1][1] === board[1][2]) ||
-      (board[2][0] &&
-        board[2][0] === board[2][1] &&
-        board[2][1] === board[2][2]) ||
-      (board[0][0] &&
-        board[0][0] === board[1][0] &&
-        board[1][0] === board[2][0]) ||
-      (board[0][1] &&
-        board[0][1] === board[1][1] &&
-        board[1][1] === board[2][1]) ||
-      (board[0][2] &&
-        board[0][2] === board[1][2] &&
-        board[1][2] === board[2][2]) ||
-      (board[0][0] &&
-        board[0][0] === board[1][1] &&
-        board[1][1] === board[2][2]) ||
-      (board[0][2] &&
-        board[0][2] === board[1][1] &&
-        board[1][1] === board[2][0]);
+      (Gameboard.board[0][0] &&
+        Gameboard.board[0][0] === Gameboard.board[0][1] &&
+        Gameboard.board[0][1] === Gameboard.board[0][2]) ||
+      (Gameboard.board[1][0] &&
+        Gameboard.board[1][0] === Gameboard.board[1][1] &&
+        Gameboard.board[1][1] === Gameboard.board[1][2]) ||
+      (Gameboard.board[2][0] &&
+        Gameboard.board[2][0] === Gameboard.board[2][1] &&
+        Gameboard.board[2][1] === Gameboard.board[2][2]) ||
+      (Gameboard.board[0][0] &&
+        Gameboard.board[0][0] === Gameboard.board[1][0] &&
+        Gameboard.board[1][0] === Gameboard.board[2][0]) ||
+      (Gameboard.board[0][1] &&
+        Gameboard.board[0][1] === Gameboard.board[1][1] &&
+        Gameboard.board[1][1] === Gameboard.board[2][1]) ||
+      (Gameboard.board[0][2] &&
+        Gameboard.board[0][2] === Gameboard.board[1][2] &&
+        Gameboard.board[1][2] === Gameboard.board[2][2]) ||
+      (Gameboard.board[0][0] &&
+        Gameboard.board[0][0] === Gameboard.board[1][1] &&
+        Gameboard.board[1][1] === Gameboard.board[2][2]) ||
+      (Gameboard.board[0][2] &&
+        Gameboard.board[0][2] === Gameboard.board[1][1] &&
+        Gameboard.board[1][1] === Gameboard.board[2][0]);
 
-    console.table(board);
+    console.table(Gameboard.board);
 
     if (win) {
       console.log(`${currentTurn} wins`);
+      displayResults("win");
+      tiles.forEach((tile) => {
+        tile.removeEventListener("click", placeToken);
+      });
       return;
     }
 
-    if (!board.some((row) => row.includes(null))) {
+    if (!Gameboard.board.some((row) => row.includes(null))) {
       console.log("Draw");
+      displayResults("draw");
       return;
     }
 
@@ -52,25 +87,64 @@ const Gameboard = (function () {
   };
 
   const play = (rowIndex, colIndex) => {
-    if (board[rowIndex][colIndex]) return;
+    if (Gameboard.board[rowIndex][colIndex]) return;
 
-    board[rowIndex][colIndex] = currentTurn;
+    Gameboard.board[rowIndex][colIndex] = currentTurn;
     turnCount++;
     checkWin();
+    displayToken(tiles[3 * rowIndex + colIndex], currentTurn);
     currentTurn = tokens[turnCount % 2];
   };
 
-  return { board, play };
+  tiles.forEach((tile) => {
+    tile.addEventListener("click", placeToken);
+  });
+
+  resetBtn.addEventListener("click", function() {
+    tiles.forEach((tile) => {
+      tile.replaceChildren();
+    });
+
+    Gameboard.board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+
+    tiles.forEach((tile) => {
+      tile.addEventListener("click", placeToken);
+    });
+
+    turnCount = 0;
+    currentTurn = tokens[turnCount % 2];
+    displayResults();
+
+    console.table(Gameboard.board);
+  });
+
+  return { play };
 })();
+
+function displayToken(tile, currentTurn) {
+  const token = document.createElement("img");
+  token.className = "token";
+  token.src =
+    currentTurn === "X"
+      ? "assets/images/xmark.svg"
+      : "assets/images/circle.svg";
+  token.alt = currentTurn;
+
+  tile.appendChild(token);
+}
 
 console.table(Gameboard.board);
 
-Gameboard.play(0, 2);
-Gameboard.play(1, 1);
-Gameboard.play(0, 1);
-Gameboard.play(0, 0);
-Gameboard.play(2, 2);
-Gameboard.play(1, 2);
-Gameboard.play(1, 0);
-Gameboard.play(2, 1);
-Gameboard.play(2, 0);
+// Game.play(0, 2);
+// Game.play(1, 1);
+// Game.play(0, 1);
+// Game.play(0, 0);
+// Game.play(2, 2);
+// Game.play(1, 2);
+// Game.play(1, 0);
+// Game.play(2, 1);
+// Game.play(2, 0);
